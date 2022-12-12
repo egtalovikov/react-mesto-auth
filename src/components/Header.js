d328f75a8506c43cdab7e3e95fabe8763bed136f
Egor Talovikov <egtalovikov@gmail.com>
2022-12-12 11:23:48 +0300

    feat: добавил бургер-меню на мобильной версии сайта

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import logo from '../logo.svg';
 
@@ -9,17 +10,38 @@ function Header({ headerLink, email, loggedIn, setLoggedIn }) {
     setLoggedIn(false);
   }
 
+  const [isMenuButtonClick, setIsMenuButtonClick] = React.useState(false);
+
+  function handleMenuButtonClick() {
+    setIsMenuButtonClick(!isMenuButtonClick);
+  }
+
   return (
     <header className="header">
-      <img src={logo} alt="Логотип Mesto Russia" className="header__logo" />
       {loggedIn ? (
-        <div className='header__signout'>
+        <div className={`header__menu ${isMenuButtonClick ? 'header__menu_active' : ''}`}>
           <p className="header__email">{email}</p>
           <button onClick={signOut} className='header__signout-button buttons'>Выйти</button>
         </div>
-      ) : (
-        <Link to={headerLink.link} className="header__link">{headerLink.title}</Link>
-      )}
+      ) : ''}
+      <div className="header__container">
+        <img src={logo} alt="Логотип Mesto Russia" className="header__logo" />
+        {loggedIn ? (
+          <>
+            <div onClick={handleMenuButtonClick} class="header__menu-button">
+              <span className={`header__burger-line ${isMenuButtonClick ? 'header__burger-line_active' : ''}`}></span>
+              <span className={`header__burger-line ${isMenuButtonClick ? 'header__burger-line_active' : ''}`}></span>
+              <span className={`header__burger-line ${isMenuButtonClick ? 'header__burger-line_active' : ''}`}></span>
+            </div>
+            <div className="header__signout">
+              <p className="header__email">{email}</p>
+              <button onClick={signOut} className='header__signout-button buttons'>Выйти</button>
+            </div>
+          </>
+        ) : (
+          <Link to={headerLink.link} className="header__link">{headerLink.title}</Link>
+        )}
+      </div>
     </header>
   )
 }
