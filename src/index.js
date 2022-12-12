531ca0ab17a7b1a9445c78c841c0e90580a9b4c5
Egor Talovikov <egtalovikov@gmail.com>
2022-12-12 11:44:47 +0300

    feat: добавил basename у BrowserRouter и убрал границы у кнопок

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.querySelector('.page__container'));
 root.render(
-    <BrowserRouter>
+  <BrowserRouter basename={process.env.REACT_APP_BASENAME || ""}>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
